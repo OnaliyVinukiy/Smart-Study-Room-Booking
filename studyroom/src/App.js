1cff59021becc1537275663480054602b683ba30
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-18 09:35:36 +0530

    added dependanices, and route paths

--- studyroom/src/App.js
+++ studyroom/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import BatchPreferenceChart from "./pages/BatchPreferenceChart";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
  
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/batchpreference" element={<BatchPreferenceChart />} />
       
     </Routes>
    

6ae61737ed95823f5e3be966a5c6eef66a384550
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-22 18:38:37 +0530

    modified home page

--- studyroom/src/pages/Home.jsx
+++ studyroom/src/pages/Home.jsx
@@ -198,11 +198,11 @@ const Home = () => {
         ) : (
           <>
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl text-center">Welcome to Smart Study!</h1>
-            <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Reserve a study room now</p>
+            <p className="mb-8 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 lg:px-32 text-center">Empowering Your Learning Journey: Reserve Your Study Room at NSBM Green University Now!</p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
               {inProgress === InteractionStatus.None && (
                 <button className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={() => instance.loginPopup()}>
-                  Login with Microsoft
+                  Login with Student Email
                   <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                   </svg>

5e506b2a6afc8af32fd18fecde1a6f14bd627a96
avish madushanka <avishmadushanka21@gmail.com>
2024-02-23 14:55:14 +0530

    fix footer

--- studyroom/src/components/Footer.jsx
+++ studyroom/src/components/Footer.jsx
@@ -3,36 +3,32 @@ import React from 'react';
 function Footer() {
   return (
     <div>
-   
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <footer className="bg-gray-500 shadow text-white lm:m-4 md:m-auto">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="/" class="hover:underline me-4 md:me-6">Home</a>
-                </li>
-                <li>
-                    <a href="/Booking" class="hover:underline me-4 md:me-6">My Booking</a>
-                </li>
-                <li>
-                    <a href="/About" class="hover:underline me-4 md:me-6">About Us</a>
-                </li>
-                <li>
-                    <a href="/Contact" class="hover:underline">Contact Us</a>
-                </li>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
+              <li>
+                <a href="/" className="hover:underline me-4 md:me-6">Home</a>
+              </li>
+              <li>
+                <a href="/Booking" className="hover:underline me-4 md:me-6">My Booking</a>
+              </li>
+              <li>
+                <a href="/About" className="hover:underline me-4 md:me-6">About Us</a>
+              </li>
+              <li>
+                <a href="/Contact" className="hover:underline">Contact Us</a>
+              </li>
             </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+          <span className="block text-sm text-gray-200 sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-    </div>
-</footer>
-
-
-
+      </footer>
     </div>
   );
 }

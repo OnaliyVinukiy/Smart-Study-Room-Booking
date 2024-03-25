6896094f312db3af4defda0296ac7325c7e177c2
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-25 20:51:06 +0530

    on click functions for button

--- studyroom/src/pages/Panel.jsx
+++ studyroom/src/pages/Panel.jsx
@@ -10,13 +10,13 @@ const Panel = () => {
   return (
     <div>
       <h1 className="text-center text-2xl mt-16">Welcome to the Control Panel of your Study Room!</h1>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center mt-20">
         <button
           type="button"
           className={`text-white ${locked ? 'bg-red-700 hover:bg-red-800' : 'bg-green-700 hover:bg-green-800'} focus:outline-none focus:ring-4 focus:ring-${locked ? 'red' : 'green'}-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-${locked ? 'red' : 'green'}-600 dark:hover:bg-${locked ? 'red' : 'green'}-700 dark:focus:ring-${locked ? 'red' : 'green'}-800`}
           onClick={handleClick}
         >
-          {locked ? 'Lock' : 'Unlock'}
+          {locked ? 'Lock the study room' : 'Unlock the study room'}
         </button>
       </div>
     </div>

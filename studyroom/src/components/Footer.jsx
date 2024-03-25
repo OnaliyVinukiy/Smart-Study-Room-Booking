28af72c3d242ac4c2d4a53135730f8ca71d25e94
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-25 20:42:43 +0530

    modified header and footer

--- studyroom/src/components/Footer.jsx
+++ studyroom/src/components/Footer.jsx
@@ -8,13 +8,15 @@ function Footer() {
     return null; // Return nothing if user is not logged in
   }
   return (
-    <div>
-      <footer className="bg-gray-800 shadow text-white m-4 md:m-auto">
+    
+      <div style={{ minHeight: "60vh", position: "relative" }}>
+      <footer className="p-4 bg-gray-800 md:p-8 lg:p-10 shadow text-white mb-0 absolute bottom-0 w-full">
+      
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
               <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap">SmartStudy</span>
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">NStudy</span>
             </a>
             <ul className="flex flex-wrap items-center justify-center mb-6 font-medium sm:mb-0">
               <li>
@@ -32,7 +34,7 @@ function Footer() {
             </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-          <span className="block text-sm text-gray-200 sm:text-center">© 2024 <a href="https://flowbite.com/" className="hover:underline">SmartStudy</a>. All Rights Reserved.</span>
+          <span className="block text-sm text-gray-200 sm:text-center">© 2024 <a href="https://flowbite.com/" className="hover:underline">NStudy</a>. All Rights Reserved.</span>
         </div>
       </footer>
     </div>

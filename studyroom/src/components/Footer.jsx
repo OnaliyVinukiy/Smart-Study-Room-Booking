6e51abdbd42cef9f54faee3e7bdb642a79b9246a
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-03-30 23:13:19 +0530

    modified footer logo

--- studyroom/src/components/Footer.jsx
+++ studyroom/src/components/Footer.jsx
@@ -15,7 +15,8 @@ function Footer() {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <img src="/favicon.ico" className="h-8" alt="Flowbite Logo" />
+
               <span className="self-center text-2xl font-semibold whitespace-nowrap">NStudy</span>
             </a>
             <ul className="flex flex-wrap items-center justify-center mb-6 font-medium sm:mb-0">
